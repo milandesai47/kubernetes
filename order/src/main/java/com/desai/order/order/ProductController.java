ea0bbe6d5f54c9bec4cee96efc7ca2c780ce2980
Milan Desai <desaimilan001@gmail.com>
2023-06-04 23:31:08 +0100

    Minor refectoring in endpoint to differenciate the rest call

--- order/src/main/java/com/desai/order/order/ProductController.java
+++ order/src/main/java/com/desai/order/order/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("product")
+@RequestMapping("/order/product")
 public class ProductController {
 
     @Value("${product.service.host}")
