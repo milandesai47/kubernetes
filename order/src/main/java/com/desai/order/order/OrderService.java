caf07262ef0b7e218643946a25890c54b0e0c088
Milan Desai <desaimilan001@gmail.com>
2024-06-26 11:01:02 +0100

    Update OrderService.java

--- order/src/main/java/com/desai/order/order/OrderService.java
+++ order/src/main/java/com/desai/order/order/OrderService.java
@@ -11,7 +11,8 @@ public class OrderService {
     private static final List<Order> ORDERS = Arrays.asList(
             new Order(1, 1, 1000),
             new Order(2, 2, 2000),
-            new Order(3, 3, 3000)
+            new Order(3, 3, 3000),
+            new Order(4, 4, 4000)
     );
 
     public Order getOrder(Integer orderId) {
